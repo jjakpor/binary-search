fe4f2911895e3b7862e09bd92e64b60cf40a7758
jjakpor <14021192+jjakpor@users.noreply.github.com>
2024-05-24 21:05:10 -0700

    Return index = index j  case done except for a few lemmas linking returnIndex' to binarySearch

--- BinarySearch/Basic.lean
+++ BinarySearch/Basic.lean
@@ -154,10 +154,8 @@ lemma index_i_imp_arr_i_le_target (ret_index_is_index : returnIndex' arr target 
   done
 
 
-abbrev Sorted [LinearOrder α] (arr : List α) : Prop :=
+abbrev Sorted (arr : List α) : Prop :=
   ∀(i j : Nat) (h : i < arr.length ∧ j < arr.length), arr[i]'h.left ≤ arr[j]'h.right
-
-variable [LinearOrder α] (arr : List α) (target : α)
 
 lemma inv_ret_pair : Invariant (searchPred arr target) (binarySearch (searchPred arr target) searchMid (0, List.length arr + 1)) := by
   have inv_in : Invariant (searchPred arr target) (0, List.length arr + 1) := by
@@ -169,16 +167,6 @@ lemma inv_ret_pair : Invariant (searchPred arr target) (binarySearch (searchPred
       simp only [Nat.add_eq, add_zero, lt_self_iff_false, List.getElem_eq_get, gt_iff_lt, dite_false]
   exact @invariant (searchPred arr target) (searchMid) (0, List.length arr + 1) inv_in
 
-lemma not_search_pred_ret : ¬searchPred arr target (returnIndex arr target) := (inv_ret_pair arr target).left
-
-lemma search_pred_succ_ret : searchPred arr target (returnIndex arr target + 1) := by
-  unfold returnIndex
-  sorry
-
-lemma arr_at_succ_ret_gt_arr_at_target [LinearOrder α] (arr : List α) (target : α) : arr[(returnIndex arr target) + 1]'sorry > target := by
-  unfold returnIndex
-  dsimp only [gt_iff_lt]
-  sorry
 
 def containsTargetAt (arr : List α) (target : α) (i : ℕ) : Prop :=
   ∃ (hi : i < arr.length), arr[i]'hi = target
@@ -186,14 +174,15 @@ def containsTargetAt (arr : List α) (target : α) (i : ℕ) : Prop :=
 def containsTarget (arr : List α) (target : α) : Prop :=
   ∃ (i : ℕ), containsTargetAt arr target i
 
-example : returnIndex' arr target = .before → ∀(h: i < arr.length), target < arr[i]'h := sorry
-
 lemma arr_ret_index_le_target (is_index : returnIndex' arr target = .index i) (in_bounds : i < arr.length): arr[i]'in_bounds ≤ target := by
   cases pos : returnIndex' arr target with
   | before => sorry
   | index j => sorry
   | after => sorry
   done
+
+lemma arr_succ_ret_index_gt_target (is_index : returnIndex' arr target = .index i) (succ_in_bounds : i + 1 < arr.length) : arr[i + 1] > target := by
+  sorry
 
 lemma ret_index_index_in_bounds (is_index : returnIndex' arr target = .index i) : i < arr.length := by
   unfold returnIndex' at is_index
@@ -204,8 +193,12 @@ lemma ret_index_index_in_bounds (is_index : returnIndex' arr target = .index i) 
     . sorry
     . contradiction
 
+lemma foo : returnIndex' arr target = .index i → (_ : i + 1 < arr.length) → target < arr[i+1] := by
+  sorry
 
-theorem bsearch_finds_target_if_target_exists [LinearOrder α] (arr : List α) (target : α)
+lemma bar (i : ℕ): returnIndex' arr target = .before → ∀(h : i < arr.length), target < arr[i]'h := by sorry
+
+theorem bsearch_finds_target_if_target_exists
     (sorted : Sorted arr) (contains : containsTarget arr target) :
     ∃j, containsTargetAt arr target j ∧ returnIndex' arr target = .index j := by
     revert contains
@@ -214,33 +207,70 @@ theorem bsearch_finds_target_if_target_exists [LinearOrder α] (arr : List α) (
     simp only [not_exists] at h
     unfold containsTarget
     simp
+    -- We want to show that for every nat, the array does not contain the target at that index. So we introduce i.
     intro i
+    /- Now we consider the value of returnIndex' arr target. It is either SearchPosition.before,
+    SearchPosition.index j for some j, or SearchPosition.after -/
     cases ret_index_eq : returnIndex' arr target with
-    | before => sorry
+    | before => -- If returnIndex' is before, that means the target is less than everything in the array.
+      have := bar _ _ i ret_index_eq -- bar means target is less than everything
+      -- Start golf section. Try to make one line
+      unfold containsTargetAt
+      simp only [not_exists]
+      intro h
+      have arr_i_ne_target := ne_of_lt (this h)
+      intro
+      have := arr_i_ne_target.symm
+      contradiction
+      -- End golf section
     | index j =>
+      -- Either the array does not contain the target at j or the return index does not use the index constructor with j
       have := h j
       have j_in_bounds := (ret_index_index_in_bounds arr target ret_index_eq)
       rw [not_and_or] at this
-      cases this
-      . rename_i target_not_at_j
-        obtain lt | eq | gt := Nat.lt_trichotomy i j
-        . have arr_j_le_target : arr[j] ≤ target := arr_ret_index_le_target arr target ret_index_eq _
-          have arr_j_ne_target : arr[j] ≠ target := by
-            unfold containsTargetAt at target_not_at_j
-            simp only [not_exists] at target_not_at_j
-            exact target_not_at_j j_in_bounds
-          have arr_j_lt_target : arr[j] < target := lt_of_le_of_ne arr_j_le_target arr_j_ne_target
-          have arr_i_le_arr_j : arr[i] ≤ arr[j] := sorted i j ⟨by linarith, j_in_bounds⟩
-          have arr_i_lt_target : arr[i] < target :=
-            calc
-            arr[i] ≤ arr[j] := arr_i_le_arr_j
-            arr[j] < target := arr_j_lt_target
-          have arr_i_ne_target := ne_of_lt arr_i_lt_target
-          unfold containsTargetAt
-          simp only [not_exists]
-          intro h
-          assumption
-        . rwa [eq]
-        . sorry
-      . contradiction
-    | after => sorry
+      cases this with
+      /- If the target is not at j, we consider three cases: i < j, i = j, and i > j. -/
+      | inl target_not_at_j =>
+          obtain lt | eq | gt := Nat.lt_trichotomy i j
+          -- This is the subcase where i < j, i.e., our arbitrary nat is less than the returned index.
+          . have arr_j_le_target : arr[j] ≤ target := arr_ret_index_le_target arr target ret_index_eq _
+            have arr_j_ne_target : arr[j] ≠ target := by
+              unfold containsTargetAt at target_not_at_j
+              simp only [not_exists] at target_not_at_j
+              exact target_not_at_j j_in_bounds
+            -- TODO: put these in the calc proof
+            have arr_j_lt_target : arr[j] < target := lt_of_le_of_ne arr_j_le_target arr_j_ne_target
+            have arr_i_le_arr_j : arr[i] ≤ arr[j] := sorted i j ⟨by linarith, j_in_bounds⟩
+            have arr_i_lt_target : arr[i] < target := -- We tried to use linarith instead of calc, but we don't know why it failed.
+              calc
+              arr[i] ≤ arr[j] := arr_i_le_arr_j
+              arr[j] < target := arr_j_lt_target
+            have arr_i_ne_target := ne_of_lt arr_i_lt_target
+            unfold containsTargetAt
+            simp only [not_exists]
+            intro h
+            assumption
+          -- This is the subcase where i = j, i.e., our arbitrary nat is equal to the returned index.
+          . rwa [eq]
+          -- This is the subcase where i > j, i.e., our arbitrary nat is greater than the returned index.
+          . have i_ge_succ_j : j + 1 ≤ i := by linarith
+            by_cases h : i < arr.length
+            -- If i is in bounds, then j + 1 is also in bounds. arr[j+1] > target by foo. Since arr is sorted, arr[i] > arr[j+1]. So arr[i] > target
+            have arr_i_ge_target : target < arr[i] :=
+              calc
+              target < arr[j+1] := foo _ _ ret_index_eq (by linarith)
+              arr[j+1] ≤ arr[i] := sorted (j + 1) i ⟨by linarith, by assumption⟩
+            unfold containsTargetAt
+            simp only [not_exists]
+            intro h
+            exact ne_of_gt arr_i_ge_target
+            -- If i >= length, then of course arr[i] is not target. You can't index arr at i
+            . unfold containsTargetAt
+              by_contra arr_i_eq_target
+              cases arr_i_eq_target
+              contradiction
+      /- This is the case where the returned index is not j. But that contradicts our earlier assumption
+         that defined this case in the first place. -/
+      | inr _ => contradiction
+    | after => sorry -- This is impossible. returnIndex' always corresponds to binary search's first returned item, which the searchPred should be false on
+    done
